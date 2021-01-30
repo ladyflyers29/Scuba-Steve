8b0e63e91d754523879f6b0ddc269e90bb74241d
ladyflyers29 <42610874+ladyflyers29@users.noreply.github.com>
2021-01-30 17:30:33 -0600

    Item Grabbing

---
+++ Scuba Steve Game/Assets/BrookeTesting/GrabObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabObject : MonoBehaviour
+{
+    public bool haveItem;
+
+    private void OnTriggerEnter(Collider Col)
+    {
+        if (Col.tag == "Item")
+        {
+            Debug.Log("You got me");
+            Col.gameObject.SetActive(false);
+            Debug.Log("Grabbed");
+            haveItem = true;
+         
+        }
+    }
+}
---
+++ Scuba Steve Game/Assets/BrookeTesting/RandomSpawn.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawn : MonoBehaviour
+{
+    public int oxygenLevel;
+    public GameObject[] spawnLocations;
+    //    private int currentLocation = 0;
+    private void Start()
+    {
+        for(int i = 0; i <= (oxygenLevel - 1); i++)
+        {
+            spawnLocations[i].SetActive(false);
+        }
+    }
+
+    //This is for testing the spawning
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if(other.tag == "Player")
+    //    {
+    //        RandomSpawnLocation();
+    //    }
+    //}
+
+    public void RandomSpawnLocation()
+    {
+        int locationNumber = Random.Range(0, (oxygenLevel - 1));
+        Debug.Log(locationNumber);
+        spawnLocations[locationNumber].gameObject.SetActive(true);
+    }
+
+
+
+
+
+
+}
