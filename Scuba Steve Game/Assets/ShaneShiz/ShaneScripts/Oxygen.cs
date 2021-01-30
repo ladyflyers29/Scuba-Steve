ba49038ad932666b69ef00934b3b69abb4c973ca
snberanek <36345444+snberanek@users.noreply.github.com>
2021-01-30 13:09:32 -0600

    oxygen
    I Breath

---
+++ Scuba Steve Game/Assets/ShaneShiz/ShaneScripts/Oxygen.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Oxygen : MonoBehaviour
+{
+    
+    public float MaxOxygen = 200;
+    public Text scoreText;
+    void Start()
+    {
+        scoreText.text = MaxOxygen.ToString("F0");  // make it a string to output to the Text object
+                                             //scoreText = GetComponent<Text>();  // if you want to reference it by code - tag it if you have several texts
+        
+    }
+
+    void Update()
+    {
+
+        MaxOxygen -= Time.deltaTime;
+        if (MaxOxygen < 0)
+        {
+            //GameOver();
+        }
+        scoreText.text = MaxOxygen.ToString("F0");
+    }
+}
