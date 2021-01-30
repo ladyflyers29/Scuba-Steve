a88cbdc7155b4b11e036afbfcb422c9525151438
snberanek <36345444+snberanek@users.noreply.github.com>
2021-01-29 22:19:04 -0600

    hope this works
    hope this works

---
+++ Scuba Steve Game/Assets/ShaneShiz/shaderstuff/shaders/camerashiz.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerashiz : MonoBehaviour
+{
+    public Material Mat;
+
+    public void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, Mat);
+    }
+}
