12f4befb15cab984c3f49bfc64a8baa3594fb834
snberanek <36345444+snberanek@users.noreply.github.com>
2021-01-31 11:13:56 -0600

    build settings
    build settings made

---
+++ Scuba Steve Game/Assets/3rdPerson+Fly/Scripts/ShopCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject maincam;
+    public GameObject shopcam;
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if(maincam.activeSelf == true)
+            {
+                maincam.SetActive(false);
+                shopcam.SetActive(true);
+            }
+
+            else if (shopcam.activeSelf == true)
+            {
+                maincam.SetActive(true);
+                shopcam.SetActive(false);
+            }
+        }
+    }
+}
