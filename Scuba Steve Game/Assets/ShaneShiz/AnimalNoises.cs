fbe4a2d6925a1bc7fa46bc3ab813bdea7cc852d1
snberanek <36345444+snberanek@users.noreply.github.com>
2021-01-31 10:22:55 -0600

    Shop Items and sea ambience
    placed the shop items into the scene and gave some of the animals noises in the water

---
+++ Scuba Steve Game/Assets/ShaneShiz/AnimalNoises.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalNoises : MonoBehaviour
+{
+    
+    public float timeLeft = 30.0f;
+    public AudioSource sound;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            sound.Play();
+            timeLeft = 30.0f;
+        }
+    }
+}
