f20bb815a268490f5d717b5c146e29fc7f27cc65
snberanek <36345444+snberanek@users.noreply.github.com>
2021-01-31 09:29:26 -0600

    animations and credits
    added animations to the shop guy, put in credits and gave a slight animation to the menu

---
+++ Scuba Steve Game/Assets/ShopKeepAnimations.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopKeepAnimations : MonoBehaviour
+{
+    public Animator anim;
+
+    private int pose = 1;
+
+    void Update()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            pose = 2;
+        }
+
+        else if (Input.GetKeyDown(KeyCode.F) && pose != 1)
+        {
+                pose = 1;
+        }
+
+
+
+        if (pose == 1)
+        {
+            anim.SetTrigger("Idle");
+            anim.ResetTrigger("Talking");
+        }
+
+        else
+        {
+            anim.SetTrigger("Talking");
+            anim.ResetTrigger("Idle");
+        }
+    }
+}
